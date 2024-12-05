76c89218ed00dcce8ce7d2132a3f3320c42dee4b
febarros06 <febarro06@gmail.com>
2024-12-05 19:31:32 -0300

    mensagem de commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-    exibirTextoNaTela('h1', 'Jogo do número secreto TESTE DE GIT PUSH ');
+    exibirTextoNaTela('h1', 'Jogo do número secreto TESTE DESAFIO');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
